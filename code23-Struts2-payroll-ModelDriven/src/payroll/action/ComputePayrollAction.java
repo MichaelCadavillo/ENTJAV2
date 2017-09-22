ed71517ef43140f1ec617e7fb22136441aaffb02
unknown <201501059@CL1-17.iacademy.loc>
2017-09-22 19:51:29 +0800

    added code 23

---
+++ code23-Struts2-payroll-ModelDriven/src/payroll/action/ComputePayrollAction.java
@@ -0,0 +1,46 @@
+package payroll.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import payroll.model.PayrollBean;
+
+public class ComputePayrollAction extends ActionSupport implements ModelDriven{
+	
+	PayrollBean payroll = new PayrollBean();
+	
+	public PayrollBean getPayroll() {
+		return payroll;
+	}
+
+	public void setPayroll(PayrollBean payroll) {
+		this.payroll = payroll;
+	}
+
+	public String execute() {
+		payroll.facade();
+		
+		return SUCCESS;
+	}
+	
+	@Override
+	public void validate(){
+		if(payroll.getHoursWorked() <= 0){
+			addFieldError("hoursWorked", "Invalid Hours.");
+		}
+		
+		if(payroll.getPayRate() <= 0){
+			addFieldError("payRate", "Invalid PayRate.");
+		}
+		
+		if(payroll.getName().trim().length() == 0){
+			addFieldError("name", "Please Input a name.");
+		}
+		
+	}
+
+	@Override
+	public Object getModel() {
+		return payroll;
+	}
+}
