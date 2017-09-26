1f02a30caea18ec5839ce86ea32a4633b4caae59
MichaelCadavillo <michael.cadavillo@gmail.com>
2017-09-26 14:03:47 +0800

    added code25

---
+++ code25-Struts2-payroll-ModelDriven-Hibernate/src/payroll/action/ComputePayrollAction.java
@@ -0,0 +1,75 @@
+package payroll.action;
+
+import javax.transaction.*;
+
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import payroll.model.PayrollBean;
+
+public class ComputePayrollAction extends ActionSupport implements ModelDriven<PayrollBean>{
+	
+	PayrollBean payroll = new PayrollBean();
+	
+	public PayrollBean getPayroll() {
+		return payroll;
+	}
+
+	public void setPayroll(PayrollBean payroll) {
+		this.payroll = payroll;
+	}
+
+	public String execute() {
+		payroll.facade();
+		
+		
+		//hibernate operations
+		
+		SessionFactory sessionFactory = 
+				new Configuration().configure().buildSessionFactory();
+			
+			Session session = null;
+			org.hibernate.Transaction transaction = null;
+			
+			try{
+				session = sessionFactory.openSession();
+				
+				if(session != null){
+					transaction = session.beginTransaction();
+					session.save(payroll);
+					transaction.commit();
+				}else{
+					System.out.println("Session is null");
+				}
+			}catch(Exception e){
+				transaction.rollback();
+			}
+			
+		return SUCCESS;
+	}
+	
+	@Override
+	public void validate(){
+		if(payroll.getHoursWorked() <= 0){
+			addFieldError("hoursWorked", "Invalid Hours.");
+		}
+		
+		if(payroll.getPayRate() <= 0){
+			addFieldError("payRate", "Invalid PayRate.");
+		}
+		
+		if(payroll.getName().trim().length() == 0){
+			addFieldError("name", "Please Input a name.");
+		}
+		
+	}
+
+	@Override
+	public PayrollBean getModel() {
+		return payroll;
+	}
+}
